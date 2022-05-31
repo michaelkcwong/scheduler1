21e8eb36ca1dc914d86017ae69853ccebc0f7719
michaelkcwong <wong_michael@rogers.com>
2022-05-30 23:00:20 -0400

    Add style InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss"
 
 export default function InterviewerList(props) {
   const interviewerListArray = props.interviewers.map((interviewer) => {
