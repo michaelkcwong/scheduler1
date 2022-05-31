a363263ad36db2f127833d3f2dde1afa051cbd58
michaelkcwong <wong_michael@rogers.com>
2022-05-30 22:24:47 -0400

    Create the <InterviewerListItem> file and set up a React component that returns the base HTML

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classNames from "classnames";
+
+
+export default function InterviewerListItem(props) {
+const interviewerClass = classNames("interviewers__item", {
+  'interviewers__item--selected':props.selected,
+});
+
+return (
+  <li className={interviewerClass}>
+onClick={() => props.setInterviewer(props.id)}
+<img
+key={props.id}
+className="interviewers__item-image"
+src="https://i.imgur.com/LpaY82x.png"
+alt="Sylvia Palmer"
+/>
+Sylvia Palmer
+
+  </li>
+)
+};
