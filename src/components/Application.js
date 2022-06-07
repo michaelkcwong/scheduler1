3c50af081881853ad836e60f9a4d0962db22ddda
michaelkcwong <wong_michael@rogers.com>
2022-06-07 00:04:27 -0400

    Add useState to store the days data and request days with axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,11 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -74,6 +58,13 @@ const appointmentsArray = Object.values(appointments).map(appointment => {
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const daysURL = `/api/days`
+    axios.get(daysURL).then(response => setDays(response.data))
+  }, []);
+
 
   console.log(day)
 
