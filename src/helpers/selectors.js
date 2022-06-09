f153f99ed2613de62b5dfc392c53a8831ca3be4b
michaelkcwong <wong_michael@rogers.com>
2022-06-08 22:47:05 -0400

    Creating the getInterview function, adding the fake state object and tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,10 +3,24 @@ export function getAppointmentsForDay(state, day) {
   state.days.forEach((obj) => {
     if (obj.name === day) {
       const appointmentList = obj.appointments;
-      appointmentList.forEach(i => {
+      appointmentList.forEach((i) => {
         filteredAppointments.push(state.appointments[i]);
-      })
-    };
-  })
+      });
+    }
+  });
   return filteredAppointments;
-}; 
+}
+
+export function getInterview(state, interview) {
+  if (interview) {
+    for (let i in state.interviewers) {
+      if (interview.interviewer === state.interviewers[i].id) {
+        return {
+          ...interview,
+          interviewer: state.interviewers[i],
+        };
+      }
+    }
+  }
+  return null;
+}
