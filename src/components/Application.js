e68266ad579eeb2c299f4ac8e04343bbcf0bb627
michaelkcwong <wong_michael@rogers.com>
2022-06-08 21:43:42 -0400

    Combined state, using spread oeprator and adding setDay and setDays functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -57,16 +57,19 @@ const appointmentsArray = Object.values(appointments).map(appointment => {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+  });
+
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({ ...prev, days}));
 
   useEffect(() => {
     const daysURL = `/api/days`
     axios.get(daysURL).then(response => setDays(response.data))
   }, []);
 
-
-  console.log(day)
 
   return (
     <main className="layout">
@@ -79,8 +82,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
