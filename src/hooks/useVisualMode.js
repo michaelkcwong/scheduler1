b6c029c3b48e2359b9e3575a5836104bca6613b6
michaelkcwong <wong_michael@rogers.com>
2022-06-09 22:13:39 -0400

    Add transition function, back function, and transition with replace along with the tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,30 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  const transition = (second, replace = false) => {
+    if (!replace) {
+      setHistory((prev) => {
+        const previousChange = prev;
+        previousChange.push(second);
+        setHistory(previousChange);
+      });
+    }
+    setMode(second);
+  };
+
+  const back = () => {
+    setHistory((prev) => {
+      const updated = prev;
+      if (updated.length >= 2) updated.pop();
+      setHistory(updated);
+    });
+    if (history.length >= 2) {
+      setMode(history[history.length - 2]);
+    } else {
+      setMode(history[0]);
+    }
+  };
+  return { mode, transition, back };
 }
