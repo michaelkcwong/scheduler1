97865ba8192dbee50adfdba27312ffb90af854a9
michaelkcwong <wong_michael@rogers.com>
2022-06-13 21:55:29 -0400

    Install PropTypes library and add code to the InterviewList.js that validates the interviewers Prop

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss"
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   const interviewerListArray = props.interviewers.map((interviewer) => {
@@ -14,6 +15,10 @@ export default function InterviewerList(props) {
       />
     );
   });
+
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
 
   return (
     <section className="interviewers">
