92ae5c3d0c452eb9c95471dfc8b99e0ad5f2b356
michaelkcwong <wong_michael@rogers.com>
2022-06-15 22:08:45 -0400

    Fix the can successfully save after trying to submit an empty student name test for it to pass

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { prettyDOM, render, cleanup, fireEvent } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
@@ -62,8 +62,8 @@ describe("Form", () => {
   
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
+    const { container, getByText, getByPlaceholderText, queryByText } = render(
+      <Form interviewers={interviewers} interviewer={1} onSave={onSave} />
     );
   
     fireEvent.click(getByText("Save"));
@@ -79,8 +79,9 @@ describe("Form", () => {
   
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
   
+    console.log(prettyDOM(container.firstChild))
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 
   it("calls onCancel and resets the input field", () => {
