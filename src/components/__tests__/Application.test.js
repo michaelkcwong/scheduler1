ef11cba5f03201a1d2dead522bca91ac472ae082
michaelkcwong <wong_michael@rogers.com>
2022-06-16 23:34:13 -0400

    Find specific day note that contains Monday with getAllByTestId query

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -44,6 +44,11 @@ it("loads data, books an interview and reduces the spots remaining for Monday by
   fireEvent.click(getByText(appointment, "Save"));
   expect(getByText(appointment, "SAVING")).toBeInTheDocument();
   await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+  
+  console.log(prettyDOM(day));
   
 
   console.log(prettyDOM(appointment));
