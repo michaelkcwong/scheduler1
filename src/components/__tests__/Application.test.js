e2199c8babde9d2df642c7ac5b1cf49d9efadea7
michaelkcwong <wong_michael@rogers.com>
2022-06-16 23:43:34 -0400

    Add notes on the tests for my reference

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -14,39 +14,28 @@ it("defaults to Monday and changes the schedule when a new day is selected", asy
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
-  /*
-  Render the Application.
-Wait until the text "Archie Cohen" is displayed.
-Click the "Add" button on the first empty appointment.
-Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
-Click the first interviewer in the list.
-Click the "Save" button on that same appointment.
-Check that the element with the text "Saving" is displayed.
-Wait until the element with the text "Lydia Miller-Jones" is displayed.
-Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
-*/
 
 it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-  const { container} = render(<Application />);
+  const { container} = render(<Application />); //Render the Application.
 
-  await waitForElement(() => getByText(container, "Archie Cohen"));
+  await waitForElement(() => getByText(container, "Archie Cohen")); //Wait until the text "Archie Cohen" is displayed.
 
   const appointments = getAllByTestId(container, "appointment");
   const appointment = appointments[0];
 
-  fireEvent.click(getByAltText(appointment, "Add"));
+  fireEvent.click(getByAltText(appointment, "Add")); //Click the "Add" button on the first empty appointment.
 
-  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), { //Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
     target: { value: "Lydia Miller-Jones" }
   });
-  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer")); //Click the first interviewer in the list.
 
-  fireEvent.click(getByText(appointment, "Save"));
-  expect(getByText(appointment, "SAVING")).toBeInTheDocument();
-  await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  fireEvent.click(getByText(appointment, "Save")); //Click the "Save" button on that same appointment.
+  expect(getByText(appointment, "SAVING")).toBeInTheDocument(); //Check that the element with the text "Saving" is displayed.
+  await waitForElement(() => getByText(appointment, "Lydia Miller-Jones")); //Wait until the element with the text "Lydia Miller-Jones" is displayed.
   const day = getAllByTestId(container, "day").find(day =>
     queryByText(day, "Monday")
   );
-  expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+  expect(getByText(day, "no spots remaining")).toBeInTheDocument(); //Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
   
 });
