dadcc8088273df12234c6088175ca8752e6b7a8c
michaelkcwong <wong_michael@rogers.com>
2022-06-17 23:10:23 -0400

    Add cypress script

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,12 @@
+// navigation.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+});
