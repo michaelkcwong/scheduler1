6f3326676a7f6a68d32f133033e6a17d3694f69a
michaelkcwong <wong_michael@rogers.com>
2022-06-18 21:11:01 -0400

    Add appointments.spec.js file , add should book an interview test and clicking on add button in second appointment

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,10 @@
+describe("appointments", () => {
+  it("should book an interview", () => {
+    cy.visit("/");
+    cy.contains("Monday");
+    cy.get("[alt=Add]")
+ .first()
+ .click();
+
+  });
+});
